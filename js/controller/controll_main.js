fe2bff1ba910885a078ce68706ea9a9133989081
leejunho <div51@naver.com>
2015-09-11 11:51:45 +0900

    controller_main.js 수정

--- js/controller/controll_main.js
+++ js/controller/controll_main.js
@@ -2,11 +2,26 @@ define(['base'], function(Base){
         
 	"use strict";
 
-    var contents = Base.loader('templete/default.html');
-    $('.contents').append(contents);
 
+    // html 로딩
+    var contents = Base.loader('templete/default.html');
+    //var pattern = /(^\t+<pre>)|(^\t+<\/pre>$)/gim;
+    var pattern = /(<pre>)|(<\/pre>)/gim;
+    var arrCode = contents.split(pattern);
+    var replaceIS = false;
+    var dataTxt = '';
+
+    for(var i=0; i<arrCode.length; i++){
+        if(arrCode[i] == undefined || arrCode[i] == null) arrCode[i] = '';
+        if(arrCode[i] == '<pre>') replaceIS = true;
+        if(arrCode[i] == '</pre>') replaceIS = false;
+        if(replaceIS && arrCode[i] != '<pre>') arrCode[i] = arrCode[i].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+        dataTxt += arrCode[i];
+    }
+
+    $('.contents').html(dataTxt);
     $('pre').snippet('html', {style:'ide-codewarrior'});
-    
 
     //PC & MOBILE
     if(Base.agentChk.getDevice() == 'MOBILE'){
